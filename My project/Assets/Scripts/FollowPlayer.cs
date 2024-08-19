0c57268e7e605b0d327be36312856fe96d680f4b
mrideedy <bca40584.20@bitmesra.ac.in>
2024-08-19 10:08:14 +0530

    new update to the enemy

--- My project/Assets/Scripts/FollowPlayer.cs
+++ My project/Assets/Scripts/FollowPlayer.cs
@@ -1,63 +1,55 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SceneManagement;
-
 public class FollowPlayer : MonoBehaviour
 {
     public NavMeshAgent ai;
     public List<Transform> destinations;
     public Animator aiAnim;
-    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime, sightDistance, catchDistance, chaseTime, minChaseTime, maxChaseTime, jumpscareTime;
+    public float walkSpeed, chaseSpeed, idleTime, minIdleTime, maxIdleTime, sightDistance;
     public bool walking, chasing;
     public Transform player;
     Transform currentDest;
     Vector3 dest;
-    int randNum;
+    int randNum, randNum2, randNum3;
     public int destinationAmount;
     public Vector3 rayCastOffset;
-    public string deathScene;
 
-    void Start()
+     void Start()
     {
         walking = true;
-        randNum = Random.Range(0, destinations.Count);
+        randNum = Random.Range(0, destinationAmount);
         currentDest = destinations[randNum];
     }
     void Update()
     {
         Vector3 direction = (player.position - transform.position).normalized;
         RaycastHit hit;
-        if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, sightDistance))
+        if(Physics.Raycast(transform.position + rayCastOffset, direction, out hit, sightDistance))
         {
-            if (hit.collider.gameObject.tag == "Player")
+            if(hit.collider.gameObject.tag == "Player")
             {
                 walking = false;
                 StopCoroutine("stayIdle");
                 StopCoroutine("chaseRoutine");
                 StartCoroutine("chaseRoutine");
+                aiAnim.ResetTrigger("walk");
+                aiAnim.ResetTrigger("idle");
+                aiAnim.SetTrigger("sprint");
                 chasing = true;
             }
         }
-        if (chasing == true)
+
+        if(chasing == true)
         {
             dest = player.position;
             ai.destination = dest;
-            ai.speed = chaseSpeed;
-            aiAnim.ResetTrigger("walk");
-            aiAnim.ResetTrigger("idle");
-            aiAnim.SetTrigger("sprint");
-            float distance = Vector3.Distance(player.position, ai.transform.position);
-            if (distance <= catchDistance)
+            ai.speed = walkSpeed;
+            if(ai.remainingDistance <= ai.stoppingDistance)
             {
-                player.gameObject.SetActive(false);
-                aiAnim.ResetTrigger("walk");
-                aiAnim.ResetTrigger("idle");
-                aiAnim.ResetTrigger("sprint");
-                aiAnim.SetTrigger("jumpscare");
-                StartCoroutine(deathRoutine());
-                chasing = false;
+                randNum2 = Random.Ran
             }
         }
         if (walking == true)
@@ -65,18 +57,22 @@ public class FollowPlayer : MonoBehaviour
             dest = currentDest.position;
             ai.destination = dest;
             ai.speed = walkSpeed;
-            aiAnim.ResetTrigger("sprint");
-            aiAnim.ResetTrigger("idle");
-            aiAnim.SetTrigger("walk");
             if (ai.remainingDistance <= ai.stoppingDistance)
             {
-                aiAnim.ResetTrigger("sprint");
-                aiAnim.ResetTrigger("walk");
-                aiAnim.SetTrigger("idle");
-                ai.speed = 0;
-                StopCoroutine("stayIdle");
-                StartCoroutine("stayIdle");
-                walking = false;
+                randNum2 = Random.Range(0, 2);
+                if (randNum2 == 0)
+                {
+                    randNum = Random.Range(0, destinationAmount);
+                    currentDest = destinations[randNum];
+                }
+                if (randNum2 == 1)
+                {
+                    aiAnim.ResetTrigger("walk");
+                    aiAnim.SetTrigger("idle");
+                    StopCoroutine("stayIdle");
+                    StartCoroutine("stayIdle");
+                    walking = false;
+                }
             }
         }
     }
@@ -85,21 +81,9 @@ public class FollowPlayer : MonoBehaviour
         idleTime = Random.Range(minIdleTime, maxIdleTime);
         yield return new WaitForSeconds(idleTime);
         walking = true;
-        randNum = Random.Range(0, destinations.Count);
+        randNum = Random.Range(0, destinationAmount);
         currentDest = destinations[randNum];
-    }
-    IEnumerator chaseRoutine()
-    {
-        chaseTime = Random.Range(minChaseTime, maxChaseTime);
-        yield return new WaitForSeconds(chaseTime);
-        walking = true;
-        chasing = false;
-        randNum = Random.Range(0, destinations.Count);
-        currentDest = destinations[randNum];
-    }
-    IEnumerator deathRoutine()
-    {
-        yield return new WaitForSeconds(jumpscareTime);
-        SceneManager.LoadScene(deathScene);
+        aiAnim.ResetTrigger("idle");
+        aiAnim.SetTrigger("walk");
     }
 }
